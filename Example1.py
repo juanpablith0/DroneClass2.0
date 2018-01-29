42db4b7b26365289d156ecb3516509a8ccaba231
juanpablith0 <juanpabloelizarrarazoliva@hotmail.com>
2018-01-29 16:44:11 -0600

    Versión Inicial

---
+++ Example1.py
@@ -0,0 +1,43 @@
+from dronekit import connect, VehicleMode, LocationGlobalRelative
+import time
+
+
+def arm_and_takeoff(TargetAltitude):
+	print ("Executing Takeoff")
+
+	while not drone.is_armable:
+		print ("Vehicle is not armable, waiting....")
+		time.sleep(1)
+
+	print("Ready to arm")
+	drone.mode = VehicleMode("GUIDED")
+	drone.armed = True
+
+	while not drone.armed:
+		print("waiting for arming...")     
+		time.sleep(1)
+
+
+	print ("Ready for takeoff, taking off...")
+	drone.simple_takeoff(TargetAltitude)
+
+	while True:
+	Altitude = drone.location.global_relative_frame.alt 
+	print("Altitude: ", Altitude)
+	time.sleep(1)
+
+		if Altitude >=TargetAltitude * 0.95:
+			print("Altitude reached")
+			break
+
+
+#Vehicle Connection
+drone = connect('127.0.0.1:14551', wait_ready=True)
+arm_and_takeoff(20)
+
+drone.airspeed = 10
+
+a_location = LocationGlobal(-35.362188, 149.165305)
+drone.simple_goto(a_location)
+
+time.sleep (1)
