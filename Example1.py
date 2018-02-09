2917efb8df9089a8d7f758a9efd2f9c3c38f3266
juanpablith0 <juanpabloelizarrarazoliva@hotmail.com>
2018-02-09 16:12:59 -0600

    Almost final

--- Example1.py
+++ Example1.py
@@ -17,14 +17,13 @@ def arm_and_takeoff(TargetAltitude):
 		print("waiting for arming...")     
 		time.sleep(1)
 
-
 	print ("Ready for takeoff, taking off...")
 	drone.simple_takeoff(TargetAltitude)
 
 	while True:
-	Altitude = drone.location.global_relative_frame.alt 
-	print("Altitude: ", Altitude)
-	time.sleep(1)
+		Altitude = drone.location.global_relative_frame.alt 
+		print("Altitude: ", Altitude)
+		time.sleep(1)
 
 		if Altitude >=TargetAltitude * 0.95:
 			print("Altitude reached")
@@ -35,9 +34,18 @@ def arm_and_takeoff(TargetAltitude):
 drone = connect('127.0.0.1:14551', wait_ready=True)
 arm_and_takeoff(20)
 
-drone.airspeed = 10
+drone.airspeed = 10 
+#WavePoints
+Punto_01 = LocationGlobalRelative (20.736864, -103.456317, 20)
+drone.simple_goto(Punto_01)
+print("Going to 01")
+Punto_02 = LocationGlobalRelative (20.737674, -103.456572, 20)
+drone.simple_goto(Punto_02)
+print("Going to 02")
+#Punto_03 = LocationGlobalRelative (20.736992, -103.457159, 20)
+#drone.simple_goto(Punto_03)
+#print: "Going to 03"
+#drone.battery.level
 
-a_location = LocationGlobal(-35.362188, 149.165305)
-drone.simple_goto(a_location)
 
-time.sleep (1)
+time.sleep (50)
