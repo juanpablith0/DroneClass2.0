44f6b0080fc855c4e8d53bf298fb77ff2fdd83d6
juanpablith0 <juanpabloelizarrarazoliva@hotmail.com>
2018-02-09 21:15:50 -0600

    Assignment 1

--- Example1.py
+++ Example1.py
@@ -1,7 +1,7 @@
 from dronekit import connect, VehicleMode, LocationGlobalRelative
 import time
 
-
+#Defining the altitude
 def arm_and_takeoff(TargetAltitude):
 	print ("Executing Takeoff")
 
@@ -35,17 +35,41 @@ drone = connect('127.0.0.1:14551', wait_ready=True)
 arm_and_takeoff(20)
 
 drone.airspeed = 10 
-#WavePoints
+#WavePoints, the points to where the drone will fly
+print("On my way to point 1 ...")
 Punto_01 = LocationGlobalRelative (20.736864, -103.456317, 20)
 drone.simple_goto(Punto_01)
-print("Going to 01")
+time.sleep (20)
+print("On my way to point 2 ...")
 Punto_02 = LocationGlobalRelative (20.737674, -103.456572, 20)
 drone.simple_goto(Punto_02)
-print("Going to 02")
-#Punto_03 = LocationGlobalRelative (20.736992, -103.457159, 20)
-#drone.simple_goto(Punto_03)
-#print: "Going to 03"
-#drone.battery.level
+time.sleep (20)
+print("On my way to point 3 ...")
+Punto_03 = LocationGlobalRelative (20.736992, -103.457159, 20)
+drone.simple_goto(Punto_03)
+time.sleep (20)
+print("Now back home")
+drone.mode = VehicleMode("RTL")
+time.sleep (20)
 
+#The level of the battery
+print("The battery voltage is:")
+print (drone.battery.voltage) 
 
-time.sleep (50)
+#For landing the drone
+def LandDrone():
+	print ("Landing")
+	drone.mode = VehicleMode ("LAND")
+	while True:
+		Altitude = drone.location.global_relative_frame.alt 
+		print("Altitude: ", Altitude)
+			
+
+		if Altitude <= 0:
+			print("Landed")
+			break
+
+		time.sleep (10)
+
+arm_and_takeoff(20)
+#LandDrone()(We have RTL)
